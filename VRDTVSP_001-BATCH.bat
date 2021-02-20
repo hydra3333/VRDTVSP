4f7cb4406ea88b45c8ef6ad823cbb66543721d73
hydra3333 <hydra3333@gmail.com>
2021-02-21 09:23:12 +1030

    go to v6 of vrd

--- VRDTVSP_001-BATCH.bat
+++ VRDTVSP_001-BATCH.bat
@@ -95,8 +95,8 @@ SET vrdtvsp_CMD=cscript //nologo "!vrdtvsp_vbs_script!" ^
 /destination_Folder:"!destination_mp4_Folder!" ^
 /failed_Folder:"!failed_conversion_TS_Folder!" ^
 /temp_path:"!scratch_Folder!" ^
-/vrd_version_for_qsf:5 ^
-/vrd_version_for_adscan:5 ^
+/vrd_version_for_qsf:6 ^
+/vrd_version_for_adscan:6 ^
 /do_qsf:True ^
 /do_adscan:True ^
 /do_audio_delay:True ^
