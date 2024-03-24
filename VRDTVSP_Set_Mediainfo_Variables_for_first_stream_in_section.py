677a111af6b7bd8915f208d52e8ed05c78c35aac
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2024-03-24 23:18:34 +1030

    Add files via upload

--- VRDTVSP_Set_Mediainfo_Variables_for_first_stream_in_section.py
+++ VRDTVSP_Set_Mediainfo_Variables_for_first_stream_in_section.py
@@ -4,14 +4,15 @@ import argparse
 from datetime import datetime
 import ctypes
 from ctypes import wintypes
-from ctypes import *		# for mediainfo ... load via ctypes.CDLL(r'.\MediaInfo.dll')
-from typing import Union	# for mediainfo
+from ctypes import *        # for mediainfo ... load via ctypes.CDLL(r'.\MediaInfo.dll')
+from typing import Union    # for mediainfo
 from pathlib import Path
 import json
 import xml.etree.ElementTree as ET
 import subprocess
 import pprint
-from MediaInfoDLL3 import MediaInfo, Stream, Info, InfoOption
+#from MediaInfoDLL3 import MediaInfo, Stream, Info, InfoOption
+from pymediainfo import MediaInfo
 
 def add_variable_to_list(key, value, set_cmd_list):
     set_cmd_list.append(f'SET "{key}={value}"')
@@ -20,59 +21,6 @@ def escape_special_chars(text):
     # Replace special characters with underscores.
     special_chars = r'<>|&"?*()\' '    # leave : and / alone
     return re.sub(r'[%s]' % re.escape(special_chars), '_', text)
-
-def get_general_info(mediafile, prefix):
-    # Create a MediaInfo object
-    media_info = MediaInfo()
-    # Open the media file
-    media_info.Open(mediafile)
-    # Get general information
-    general_info = media_info.Get(Stream.General, 0)
-    # Populate dictionary with key/value pairs for general information
-    general_dict = {}
-    for info_type in Info:
-        info_value = general_info.Get(info_type)
-        if info_value:
-            general_dict[escape_special_chars(prefix + Info.enum_type(info_type))] = escape_special_chars(info_value)
-    # Close the MediaInfo object
-    media_info.Close()
-    return general_dict
-
-def get_video_info(mediafile, prefix):
-    # Create a MediaInfo object
-    media_info = MediaInfo()
-    # Open the media file
-    media_info.Open(mediafile)
-    # Get information for the first video stream
-    video_info = media_info.Get(Stream.Video, 0)
-    # Populate dictionary with key/value pairs for video information
-    video_dict = {}
-    if video_info:
-        for info_type in Info:
-            info_value = video_info.Get(info_type)
-            if info_value:
-                video_dict[escape_special_chars(prefix + Info.enum_type(info_type))] = escape_special_chars(info_value)
-    # Close the MediaInfo object
-    media_info.Close()
-    return video_dict
-
-def get_audio_info(mediafile, prefix):
-    # Create a MediaInfo object
-    media_info = MediaInfo()
-    # Open the media file
-    media_info.Open(mediafile)
-    # Get information for the first audio stream
-    audio_info = media_info.Get(Stream.Audio, 0)
-    # Populate dictionary with key/value pairs for audio information
-    audio_dict = {}
-    if audio_info:
-        for info_type in Info:
-            info_value = audio_info.Get(info_type)
-            if info_value:
-                audio_dict[escape_special_chars(prefix + Info.enum_type(info_type))] = escape_special_chars(info_value)
-    # Close the MediaInfo object
-    media_info.Close()
-    return audio_dict
 
 if __name__ == "__main__":
     # eg clear, set with python3, then show
@@ -126,32 +74,49 @@ if __name__ == "__main__":
     set_cmd_list.append(f'REM First, clear the variables with the chosen prefix')
     set_cmd_list.append(f'FOR /F "tokens=1,* delims==" %%G IN (\'SET !prefix!\') DO (SET "%%G=")')
     if section_name.lower() == "General".lower():
-        general_info_dict = get_general_info(mediafile, prefix)
+        media_info = MediaInfo.parse(mediafile, parse_speed=1)    # ,full=True ,output="JSON" returns a string
+        general_track = media_info.general_tracks[0]
         print("DEBUG: General Information:")
-        for key, value in general_info_dict.items():
-            print(f"DEBUG: {key}: {value}")
-            os.environ[key] = value    # Because os.environ() ONLY set/get environment variables within the life of the PYTHON process
-            #debug_value = os.environ[key]
-            #print(f"DEBUG: after set_env_variable '{key}' = '{debug_value}'")
-            set_cmd_list.append(f'SET "{key}={value}"')
+        #for key, value in general_track.items():
+        #for key in sorted(general_track.to_data().keys()):
+        for key,value in general_track.to_data().items():
+            item_key = escape_special_chars(prefix + key)
+            item_value =  escape_special_chars(value)
+            print(f"DEBUG: General {item_key}: {item_value}")
+            os.environ[item_key] = item_value    # Because os.environ() ONLY set/get environment variables within the life of the PYTHON process
+            #debug_value = os.environ[item_key]
+            #print(f"DEBUG: after set_env_variable '{item_key}' = '{debug_value}'")
+            set_cmd_list.append(f'SET "{item_key}={item_value}"')
     elif section_name.lower() == "Video".lower():
-        video_info_dict = get_video_info(mediafile, prefix)
+        media_info = MediaInfo.parse(mediafile, parse_speed=1)    # ,full=True ,output="JSON" returns a string
+        video_track = media_info.video_tracks[0]
+        #for key, value in video_track.items():
         print("DEBUG: Video Information:")
-        for key, value in video_info_dict.items():
-            print(f"DEBUG: {key}: {value}")
-            os.environ[key] = value    # Because os.environ() ONLY set/get environment variables within the life of the PYTHON process
-            #debug_value = os.environ[key]
-            #print(f"DEBUG: after set_env_variable '{key}' = '{debug_value}'")
-            set_cmd_list.append(f'SET "{key}={value}"')
+        #for key, value in video_track.items():
+        #for key in sorted(video_track.to_data().keys()):
+        for key,value in video_track.to_data().items():
+            item_key = escape_special_chars(prefix + key)
+            item_value =  escape_special_chars(value)
+            print(f"DEBUG: Video {item_key}: {item_value}")
+            os.environ[item_key] = item_value    # Because os.environ() ONLY set/get environment variables within the life of the PYTHON process
+            #debug_value = os.environ[item_key]
+            #print(f"DEBUG: after set_env_variable '{item_key}' = '{debug_value}'")
+            set_cmd_list.append(f'SET "{item_key}={item_value}"')
     elif section_name.lower() == "Audio".lower():
-        audio_info_dict = get_audio_info(mediafile, prefix)
+        media_info = MediaInfo.parse(mediafile, parse_speed=1)    # ,full=True ,output="JSON" returns a string
+        audio_track = media_info.audio_tracks[0]
+        #for key, value in audio_track.items():
         print("DEBUG: Audio Information:")
-        for key, value in audio_info_dict.items():
-            print(f"DEBUG: {key}: {value}")
-            os.environ[key] = value    # Because os.environ() ONLY set/get environment variables within the life of the PYTHON process
-            #debug_value = os.environ[key]
-            #print(f"DEBUG: after set_env_variable '{key}' = '{debug_value}'")
-            set_cmd_list.append(f'SET "{key}={value}"')
+        #for key, value in audio_track.items():
+        #for key in sorted(audio_track.to_data().keys()):
+        for key,value in audio_track.to_data().items():
+            item_key = escape_special_chars(prefix + key)
+            item_value =  escape_special_chars(value)
+            print(f"DEBUG: Audio {item_key}: {item_value}")
+            os.environ[item_key] = item_value    # Because os.environ() ONLY set/get environment variables within the life of the PYTHON process
+            #debug_value = os.environ[item_key]
+            #print(f"DEBUG: after set_env_variable '{item_key}' = '{debug_value}'")
+            set_cmd_list.append(f'SET "{item_key}={item_value}"')
     else:
         print(f"Error: Invalid MediaInfo section '{section_name}' processing {mediafile}\nPlease specify a valid section (e.g., Video, Audio, General).")
         exit(1)
