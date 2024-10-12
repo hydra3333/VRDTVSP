11798881aeacec125ae7559b9d85a1304334e386
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2024-10-13 05:12:25 +1030

    reduce ffmpeg loglevel

--- VRDTVSP-v40-i4670-3900X.bat
+++ VRDTVSP-v40-i4670-3900X.bat
@@ -1747,7 +1747,8 @@ ECHO !DATE! !TIME! >> "!vrdlog!" 2>&1
 REM ffmpeg throws an error due to "-c:v copy" and this together: -vf "setdar="!QSF_MI_V_DisplayAspectRatio_String_slash!"
 REM ffmpeg throws an error due to "-c:v copy" and this together: -profile:v high -level 5.2 
 set "FFMPEG_cmd="!ffmpegexe64!""
-set "FFMPEG_cmd=!FFMPEG_cmd! -hide_banner -v warning -nostats -fflags +igndts+discardcorrupt -err_detect ignore_err -probesize 100M -analyzeduration 100M -strict experimental"
+REM set "FFMPEG_cmd=!FFMPEG_cmd! -hide_banner -v error -nostats -fflags +igndts+discardcorrupt -err_detect ignore_err -probesize 100M -analyzeduration 100M -strict experimental"
+set "FFMPEG_cmd=!FFMPEG_cmd! -hide_banner -v error -nostats -fflags +igndts -err_detect ignore_err -probesize 100M -analyzeduration 100M -strict experimental"
 set "FFMPEG_cmd=!FFMPEG_cmd! -i "!source_filename!" "
 set "FFMPEG_cmd=!FFMPEG_cmd! -map 0:v:0 -map 0:a:0 -map_metadata 0 -map_metadata:s:v 0:s:v -map_metadata:s:a 0:s:a"
 set "FFMPEG_cmd=!FFMPEG_cmd! -c:v copy -fps_mode passthrough"
